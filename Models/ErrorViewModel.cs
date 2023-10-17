01592f45a58f62a5ce8db45660310575b6d984be
GabiDourado <gabi.dourados2xd@gmai.com>
2023-10-17 14:13:57 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaNotasFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
